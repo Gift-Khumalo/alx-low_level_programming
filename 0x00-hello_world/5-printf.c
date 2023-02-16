c61699502b8b4e5ab79d6939699fd61ee76061f9
Gift-Khumalo <geekayheart@gmail.com>
2023-02-16 09:26:58 -0800

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Proper grammar displayed
+ *
+ * Return: 0 when complete
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}	
