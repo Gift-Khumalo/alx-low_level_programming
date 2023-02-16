59ea07f934726e3288ad517a4036a0f021a38d11
Gift-Khumalo <geekayheart@gmail.com>
2023-02-16 09:01:15 -0800

    Puts code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this will be my main funtion
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingiual puzzle");
+	return (0);
+}	
